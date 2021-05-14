974d9c5485ab1e1b16af21f3c44e9d293202b29d
Prayant <prayantgupta15@gmail.com>
2021-05-14 18:07:10 +0530

    Fixed Filter

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -2,6 +2,7 @@ import 'package:cowin/Models/CenterModel.dart';
 import 'package:cowin/methods/CenterViewModel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MyHomePage extends StatefulWidget {
   String stateName;
@@ -9,6 +10,7 @@ class MyHomePage extends StatefulWidget {
   String districtId;
   String stateId;
   String date;
+
   MyHomePage({
     @required this.stateName,
     @required this.stateId,
@@ -24,6 +26,11 @@ class _MyHomePageState extends State<MyHomePage> {
   ScrollController scrollController = ScrollController();
 
   bool loading = true;
+
+  bool _eighteen = true;
+  bool _forty5 = true;
+  bool cvld = true;
+  bool cvx = true;
 
   Widget myCard(CenterBlockModel centerBlockModel) {
     return Card(
@@ -62,13 +69,11 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Text(centerBlockModel.name,
-                    style: TextStyle(fontWeight: FontWeight.w500)),
+                child: Text(centerBlockModel.name, style: TextStyle(fontWeight: FontWeight.w500)),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Text(centerBlockModel.address,
-                    style: TextStyle(color: Colors.grey)),
+                child: Text(centerBlockModel.address, style: TextStyle(color: Colors.grey)),
               ),
               SizedBox(height: 20),
               Padding(
@@ -80,12 +85,8 @@ class _MyHomePageState extends State<MyHomePage> {
                         Text(
                             centerBlockModel.sessions[0].minAgeLimit == null
                                 ? 'NA'
-                                : centerBlockModel.sessions[0].minAgeLimit
-                                        .toString() +
-                                    '+',
-                            style: TextStyle(
-                                color: Colors.red,
-                                fontWeight: FontWeight.w500)),
+                                : centerBlockModel.sessions[0].minAgeLimit.toString() + '+',
+                            style: TextStyle(color: Colors.red, fontWeight: FontWeight.w500)),
                         Text("Age", style: TextStyle(color: Colors.grey)),
                       ],
                     ),
@@ -93,14 +94,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     Column(
                       children: [
                         Text(
-                            centerBlockModel.sessions[0].availableCapacity ==
-                                    null
+                            centerBlockModel.sessions[0].availableCapacity == null
                                 ? 'NA'
-                                : centerBlockModel.sessions[0].availableCapacity
-                                    .toString(),
-                            style: TextStyle(
-                                color: Colors.red,
-                                fontWeight: FontWeight.w500)),
+                                : centerBlockModel.sessions[0].availableCapacity.toString(),
+                            style: TextStyle(color: Colors.red, fontWeight: FontWeight.w500)),
                         Text("Doses", style: TextStyle(color: Colors.grey)),
                       ],
                     ),
@@ -117,14 +114,12 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text("Slots Available:",
-                          style: TextStyle(color: Colors.grey)),
+                      Text("Slots Available:", style: TextStyle(color: Colors.grey)),
                       SizedBox(height: 5),
-                      for (int i = 0;
-                          i < centerBlockModel.sessions[0].slots.length;
-                          i++)
+                      for (int i = 0; i < centerBlockModel.sessions[0].slots.length; i++)
                         Column(
                           children: [
+                            SizedBox(height: 2),
                             Text(centerBlockModel.sessions[0].slots[i]),
                           ],
                         ),
@@ -139,11 +134,10 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _loadData() async {
-    await CenterViewModel.getCentresByDistrict(
-        distId: widget.districtId, date: widget.date);
+    await CenterViewModel.getCentresByDistrict(distId: widget.districtId, date: widget.date);
   }
 
- @override
+  @override
   void initState() {
     // TODO: implement initState
     _loadData();
@@ -156,105 +150,277 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-   
     return SafeArea(
       child: Scaffold(
-          body: Stack(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height * 0.5,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(88)),
+        body: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.5,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.vertical(bottom: Radius.circular(88)),
+              ),
             ),
+            FutureBuilder(
+              future: CenterViewModel.getCentresByDistrict(distId: widget.districtId, date: widget.date),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  CentresModel cm = snapshot.data;
+                  print(cm.centers.length.toString());
+                  print("data");
+                  return CupertinoScrollbar(
+                    //MAIN LIST VIEW
+                    child: ListView(padding: EdgeInsets.symmetric(horizontal: 12), children: [
+                      header(),
+                      SizedBox(height: 25),
+                      FilterMenu(),
+                      ListView.builder(
+                          padding: EdgeInsetsDirectional.only(top: 10, bottom: 20),
+                          shrinkWrap: true,
+                          itemCount: cm.centers.length,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemBuilder: (context, i) {
+                            bool show18 = false;
+                            bool show45 = false;
+                            bool showCovx = false;
+                            bool showCovd = false;
+                            if (cvx) {
+                              if (cm.centers[i].sessions[0].vaccine.toLowerCase().startsWith("covax"))
+                                showCovx = true;
+                              else
+                                showCovx = false;
+                            }
+
+                            if (cvld) {
+                              if (cm.centers[i].sessions[0].vaccine.toLowerCase().startsWith('covi'))
+                                showCovd = true;
+                              else
+                                showCovd = false;
+                            }
+
+                            if (_eighteen) {
+                              if (cm.centers[i].sessions[0].minAgeLimit == 18)
+                                show18 = true;
+                              else
+                                show18 = false;
+                            }
+
+                            if (_forty5) {
+                              if (cm.centers[i].sessions[0].minAgeLimit == 45)
+                                show45 = true;
+                              else
+                                show45 = false;
+                            }
+                            if ((show18 || show45) && (showCovd || showCovx))
+                              return myCard(cm.centers[i]);
+                            else
+                              return Container();
+                          }),
+                    ]),
+                  );
+                } else
+                  return Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 12),
+                    child: Column(
+                      children: [
+                        header(),
+
+                        //TODO:ADDING SOME LOADING GIF
+                        CircularProgressIndicator(
+                          backgroundColor: Colors.white,
+                        ),
+                      ],
+                    ),
+                  );
+              },
+            ),
+          ],
+        ),
+
+        //     Stack(
+        //   children: [
+        //     Container(
+        //       height: MediaQuery.of(context).size.height * 0.5,
+        //       decoration: BoxDecoration(
+        //         color: Colors.blue,
+        //         borderRadius: BorderRadius.vertical(bottom: Radius.circular(88)),
+        //       ),
+        //     ),
+        //     CupertinoScrollbar(
+        //       controller: scrollController,
+        //       child: ListView(
+        //         controller: scrollController,
+        //         padding: EdgeInsets.symmetric(horizontal: 20),
+        //         children: [
+        //           SizedBox(height: 20),
+        //           Align(
+        //             alignment: Alignment.topLeft,
+        //             child: InkWell(
+        //               onTap: () {
+        //                 Navigator.pop(context);
+        //               },
+        //               child: Icon(Icons.arrow_back_ios, color: Colors.white),
+        //             ),
+        //           ),
+        //           SizedBox(height: 20),
+        //           Row(
+        //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //             children: [
+        //               Flexible(
+        //                   child: Text("${widget.districtName},",
+        //                       style: TextStyle(
+        //                         color: Colors.white,
+        //                         fontSize: 40,
+        //                         fontWeight: FontWeight.w500,
+        //                       ))),
+        //               Text("Fri, 8 Mar",
+        //                   style: TextStyle(color: Colors.white, fontSize: 20)),
+        //             ],
+        //           ),
+        //           Text("${widget.stateName}",
+        //               style: TextStyle(
+        //                 color: Colors.white,
+        //                 fontSize: 35,
+        //                 fontWeight: FontWeight.bold,
+        //               )),
+        //           // loading
+        //           //     ? CircularProgressIndicator()
+        //           //     : CenterViewModel.centres.length == 0
+        //           //         ? Text("No Appoinemtns")
+        //           //         : ListView.builder(
+        //           //             shrinkWrap: true,
+        //           //             itemCount: CenterViewModel.centres.length,
+        //           //             physics: NeverScrollableScrollPhysics(),
+        //           //             itemBuilder: (context, i) =>
+        //           //                 myCard(CenterViewModel.centres[i]),
+        //           //
+        //           //             // ------------------------------------------------------
+        //           //
+        //           //           ),
+        //
+        //
+        //
+        //
+        //         ],
+        //       ),
+        //     )
+        //   ],
+        // )
+      ),
+    );
+  }
+
+  FilterMenu() {
+    double borderRadius = 12;
+    Color red = Colors.red;
+    Color white = Colors.white;
+    Color black = Colors.black;
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          children: [
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(borderRadius),
+              ),
+              color: _eighteen ? red : white,
+              child: Text("18+", style: TextStyle(color: _eighteen ? white : black, fontWeight: FontWeight.w600)),
+              onPressed: () {
+                _eighteen = !_eighteen;
+                HapticFeedback.lightImpact();
+                setState(() {});
+              },
+            ),
+            SizedBox(width: MediaQuery.of(context).size.width * 0.1),
+            RaisedButton(
+              color: _forty5 ? red : white,
+              child: Text(
+                "45+",
+                style: TextStyle(
+                  color: _forty5 ? white : black,
+                ),
+              ),
+              onPressed: () {
+                _forty5 = !_forty5;
+                HapticFeedback.lightImpact();
+                setState(() {});
+              },
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            RaisedButton(
+              color: cvx ? red : white,
+              child: Text(
+                "Covaxin",
+                style: TextStyle(
+                  color: cvx ? white : black,
+                ),
+              ),
+              onPressed: () {
+                cvx = !cvx;
+                HapticFeedback.lightImpact();
+                setState(() {});
+              },
+            ),
+            SizedBox(width: MediaQuery.of(context).size.width * 0.1),
+            RaisedButton(
+              color: cvld ? red : white,
+              child: Text(
+                "Covishield",
+                style: TextStyle(
+                  color: cvld ? white : black,
+                ),
+              ),
+              onPressed: () {
+                cvld = !cvld;
+                HapticFeedback.lightImpact();
+                setState(() {});
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  header() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 20),
+        Align(
+          alignment: Alignment.topLeft,
+          child: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(Icons.arrow_back_ios, color: Colors.white),
           ),
-          CupertinoScrollbar(
-            controller: scrollController,
-            child: ListView(
-              controller: scrollController,
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              children: [
-                SizedBox(height: 20),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Icon(Icons.arrow_back_ios, color: Colors.white),
-                  ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Flexible(
-                        child: Text("${widget.districtName},",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 40,
-                              fontWeight: FontWeight.w500,
-                            ))),
-                    Text("Fri, 8 Mar",
-                        style: TextStyle(color: Colors.white, fontSize: 20)),
-                  ],
-                ),
-                Text("${widget.stateName}",
+        ),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Flexible(
+                child: Text("${widget.districtName},",
                     style: TextStyle(
                       color: Colors.white,
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
-                    )),
-                loading
-                    ? CircularProgressIndicator()
-                    : CenterViewModel.centres.length == 0
-                        ? Text("No Appoinemtns")
-                        : ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: CenterViewModel.centres.length,
-                            physics: NeverScrollableScrollPhysics(),
-                            itemBuilder: (context, i) =>
-                                myCard(CenterViewModel.centres[i]),
-
-                            // ------------------------------------------------------
-
-                            // FutureBuilder(
-                            //   future: CenterViewModel.getCentresByDistrict(distId: widget.districtId),
-                            //   builder: (context, snapshot) {
-                            //     switch (snapshot.connectionState) {
-                            //       case ConnectionState.waiting:
-                            //         print("Waiting");
-                            //         break;
-                            //       case ConnectionState.done:
-                            //         print("Done");
-                            //
-                            //         break;
-                            //       case ConnectionState.none:
-                            //         print("None");
-                            //         break;
-                            //       case ConnectionState.active:
-                            //         print("Active");
-                            //         break;
-                            //     }
-                            //     if (snapshot.hasData) {
-                            //       CenterModel cm = snapshot.data;
-                            //       print(cm.centers.length.toString());
-                            //       print("data");
-                            //       return ListView.builder(
-                            //           shrinkWrap: true,
-                            //           itemCount: cm.centers.length,
-                            //           physics: NeverScrollableScrollPhysics(),
-                            //           itemBuilder: (context, i) => myCard(cm.centers[i]));
-                            //     } else
-                            //       return Container();
-                            //   },
-                            // ),
-                          )
-              ],
-            ),
-          )
-        ],
-      )),
+                      fontSize: 40,
+                      fontWeight: FontWeight.w500,
+                    ))),
+            Text("Fri, 8 Mar", style: TextStyle(color: Colors.white, fontSize: 20)),
+          ],
+        ),
+        Text("${widget.stateName}",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+            )),
+      ],
     );
   }
 }
--- lib/methods/CenterViewModel.dart
+++ lib/methods/CenterViewModel.dart
@@ -5,21 +5,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 class CenterViewModel {
-  static List<CenterBlockModel> centres = [];
-  static Future getCentresByDistrict(
-      {@required String distId, @required String date}) async {
-    centres = [];
-    String url =
-        'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict'
+  static Future<CentresModel> getCentresByDistrict({@required String distId, @required String date}) async {
+    String url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict'
         '?district_id=${distId}&date=${date}';
     print(url);
     http.Response res = await http.get(url);
     switch (res.statusCode) {
       case 200:
         print(jsonDecode(res.body));
+
         CentresModel cm = CentresModel.fromJson(jsonDecode(res.body));
+
         print("length" + cm.centers.length.toString());
-        for (int i = 0; i < cm.centers.length; i++) centres.add(cm.centers[i]);
+        return cm;
+        // for (int i = 0; i < cm.centers.length; i++) centres.add(cm.centers[i]);
         break;
       case 401:
         print("Auth Error");
